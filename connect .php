3f7e7ba8472c8d0dc4294dddd0529a8f245eebd2
dbenta <62037747+dbenta@users.noreply.github.com>
2021-03-26 10:33:28 -0400

    commit

--- connect .php
+++ connect .php
@@ -2,11 +2,11 @@
 
 $servername = 'localhost';
 $username = 'root';
-$password = 'root';
+/*$password = 'mysql';*/
 $db = 'goals';
 $port = 8889;
 
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($servername, $username, $db);
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
