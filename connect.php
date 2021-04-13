272f487e53ab625b6efc667e66f2e5de6250cf19
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 12:25:53 -0400

    selected database

--- connect.php
+++ connect.php
@@ -10,4 +10,6 @@ if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
 
+mysqli_select_db($conn, $goals);
+
 ?>
--- insert_goal.php
+++ insert_goal.php
@@ -4,7 +4,7 @@ include 'connect.php';
 
 $query = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_progress) VALUES ('category', 'text', 'date', 'progress')";
 
-if(mysqli_query($link, $sql)){
+if(mysqli_query($conn, $query)){
     print ("Stored");
 } else {
     print ("Failed");
