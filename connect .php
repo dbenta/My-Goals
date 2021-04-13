2b5f8f088c82ec9c227fb0c806f5b34669c4448c
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 11:25:55 -0400

    added password

--- connect .php
+++ connect .php
@@ -4,9 +4,8 @@ $servername = 'localhost';
 $username = 'root';
 $password = 'password';
 $db = 'goals';
-$port = 8889;
 
-$conn = new mysqli($servername, $password,  $username, $db);
+$conn = new mysqli($servername, $password, $username, $db);
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
