9cebbc3d7c829fd0495f68d7efa3eff4afa9f0f8
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 11:52:21 -0400

    fixed name of file

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'goals';
+
+$conn = new mysqli('localhost', 'root', 'password', 'goals');
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+?>
