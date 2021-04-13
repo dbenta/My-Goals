ca536b7a1ffbc333569b2079dd73acfca65853d9
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 11:20:32 -0400

    added password

--- connect .php
+++ connect .php
@@ -6,7 +6,7 @@ $password = 'password';
 $db = 'goals';
 $port = 8889;
 
-$conn = new mysqli($servername, $username, $db);
+$conn = new mysqli($servername, $password,  $username, $db);
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
