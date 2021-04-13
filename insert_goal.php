50aafee55b022ad09011352ce4ec0ddd875a7b64
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 12:51:32 -0400

    changed insert included to require once

--- insert_goal.php
+++ insert_goal.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'connect.php';
+require_once 'connect.php';
 
 $query = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_progress) VALUES ('$_POST[category]', '$_POST[text]', 'd$_POST[date]', '$_POST[progress]')";
 
