ddefd99d73e39443c33ec56822c8b1305f047973
dbenta <62037747+dbenta@users.noreply.github.com>
2021-04-13 12:52:02 -0400

    fixed mistype

--- insert_goal.php
+++ insert_goal.php
@@ -2,7 +2,7 @@
 
 require_once 'connect.php';
 
-$query = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_progress) VALUES ('$_POST[category]', '$_POST[text]', 'd$_POST[date]', '$_POST[progress]')";
+$query = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_progress) VALUES ('$_POST[category]', '$_POST[text]', '$_POST[date]', '$_POST[progress]')";
 
 if($mysqli->query($query) === true){
     print ("Stored");
